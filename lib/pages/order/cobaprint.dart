7ae5f02c6da907cdb5c6e560b9233735fa7ee30b
agitha <e41221692@student.polije.ac.id>
2025-05-26 08:18:29 +0700

    update versi supa_flutter

---
+++ lib/pages/order/cobaprint.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../services/prindapur.dart';
+// import 'printdetil.dart';
+// import 'printdapur.dart';
+
+class cobaprint extends StatefulWidget {
+  const cobaprint({super.key});
+
+  @override
+  State<cobaprint> createState() =>
+      _cobaprint();
+}
+
+class _cobaprint extends State<cobaprint> {
+  final supabase = Supabase.instance.client;
+  List<Map<String, dynamic>> orders = [];
+  bool loading = true;
+  String searchQuery = '';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders();
+  }
+
+  Future<void> fetchOrders() async {
+    setState(() {
+      loading = true;
+    });
+
+    try {
+      final data = await supabase
+          .from('history_kasir')
+          .select()
+
+          .ilike('nama_pelanggan', '%$searchQuery%');
+
+      orders = (data as List<dynamic>)
+          .map((row) => Map<String, dynamic>.from(row))
+          .toList();
+    } catch (e) {
+      print('Error fetch: $e');
+      orders = [];
+    }
+
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+        const Text('Konfirmasi Pesanan', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.green,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            // Search Bar
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Cari Nama Pelanggan',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 12, vertical: 0),
+              ),
+              onChanged: (value) {
+                searchQuery = value;
+                fetchOrders();
+              },
+            ),
+            const SizedBox(height: 12),
+            Expanded(
+              child: loading
+                  ? const Center(child: CircularProgressIndicator())
+                  : orders.isEmpty
+                  ? const Center(child: Text('Tidak ada pesanan.'))
+                  : ListView.builder(
+                itemCount: orders.length,
+                itemBuilder: (context, index) {
+                  final order = orders[index];
+                  final idPemesanan = order['order_no'] ?? '';
+                  final nama = order['nama_pelanggan'] ?? '';
+                  final total = order['total_harga'] ?? 0;
+
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    margin: const EdgeInsets.symmetric(vertical: 6),
+                    elevation: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Nama Customer: $nama',
+                              style: const TextStyle(fontSize: 16)),
+                          const SizedBox(height: 4),
+                          Text('Order No: $idPemesanan',
+                              style: const TextStyle(fontSize: 16)),
+                          const SizedBox(height: 4),
+                          Text('Total Harga: Rp $total',
+                              style: const TextStyle(fontSize: 16)),
+                          const SizedBox(height: 12),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 40,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                  BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: () async {
+                                final result = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => prindapur(
+                                      order_no : idPemesanan,
+                                    ),
+                                  ),
+                                );
+
+                                // Jika berhasil dikonfirmasi di halaman detail
+                                if (result == true) {
+                                  await fetchOrders();
+                                }
+                              },
+                              child: const Text('Konfirmasi',
+                                  style: TextStyle(fontSize: 16)),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,19 +6,19 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import blue_thermal_printer
 import path_provider_foundation
 import printing
 import shared_preferences_foundation
 import sign_in_with_apple
 import url_launcher_macos
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  BlueThermalPrinterPlugin.register(with: registry.registrar(forPlugin: "BlueThermalPrinterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
